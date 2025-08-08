4e2cc2c5cd8a7f6a58c51da86421325c4d0a4710
Abhijeet Rao <abhijeetrao7800@gmail.com>
2025-08-08 23:07:28 +0530

    changes commit

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,7 @@ import {Router} from 'express'
 import {registerUser} from "../controllers/user.controllers.js"
 const router = Router()
 
-router.route("api/v1/users/register").post(registerUser)
+// router.route("api/v1/users/register").post(registerUser)
+router.post("/register", registerUser)
 
 export default router;
